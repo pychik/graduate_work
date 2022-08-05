5e6a71cdbbfb0dc1f1d177fc3c31127db0c83a02
vitalijsofronuk <v.sofronyuk@puls.ru>
2022-08-05 15:09:20 +0300

    fix entrypoint.sh

--- ugc/banchmark/entrypoint.sh
+++ ugc/banchmark/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 cmd="$@"
-while true;
-if ! nc -z -v $CH_HOST $CH_PORT and ! nc -z -v $MONGO_HOST $MONGO_PORT (goto done) else (goto do)
+ACCESS=false
+
+while ! $ACCESS;
 do
-  >&2 echo "Databases is not available, waiting 3 sec!"
-  sleep 3;
+  CONNECT_CH= nc -z -v $CH_HOST $CH_PORT
+  CONNECT_MD= nc -z -v $MONGO_HOST $MONGO_PORT
+  if [ ! CONNECT_CH ] && [ ! CONNECT_MD ];
+  then
+    >&2 echo "Databases is not available, waiting 3 sec!"
+    sleep 3;
+  else
+    ACCESS=true
+  fi
 done
 exec
 $cmd
