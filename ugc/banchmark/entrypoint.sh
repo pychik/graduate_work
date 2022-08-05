535bbe16aee5689798d85f2424df1f037c620bab
vitalijsofronuk <v.sofronyuk@puls.ru>
2022-08-05 15:47:22 +0300

    Fix error

--- ugc/banchmark/entrypoint.sh
+++ ugc/banchmark/entrypoint.sh
@@ -4,12 +4,14 @@ ACCESS=false
 
 while ! $ACCESS;
 do
-  CONNECT_CH= nc -z -v $CH_HOST $CH_PORT
-  CONNECT_MD= nc -z -v $MONGO_HOST $MONGO_PORT
-  if [ ! CONNECT_CH ] && [ ! CONNECT_MD ];
-  then
-    >&2 echo "Databases is not available, waiting 3 sec!"
-    sleep 3;
+  if ! nc -z -v $CH_HOST $CH_PORT
+    then
+      >&2 echo "ClickHouse is not available, waiting 3 sec!"
+      sleep 3;
+  elif ! nc -z -v $MONGO_HOST $MONGO_PORT
+    then
+      >&2 echo "MongoDB is not available, waiting 3 sec!"
+      sleep 3;
   else
     ACCESS=true
   fi
