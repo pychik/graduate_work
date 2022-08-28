f8315f1bec54c7301856ccf645b72c26033ffa0e
pychik <gendalf.grey2014@yandex.ru>
2022-08-28 16:53:07 +0300

    sender to discuss

--- notification/app/models.py
+++ notification/app/models.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel
 
 
-class DataModel(BaseModel):
-    template: str
-    user_list: list[UserData]
-
-
 class UserData(BaseModel):
     id: int
     notify: bool
     template: str
     email: str
     subject: str
+
+class DataModel(BaseModel):
+    template: str
+    user_list: list[UserData]
+
+
