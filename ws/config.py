070e18fd5d47bc68d7c1c7a746b62f4813324df6
pychik <gendalf.grey2014@yandex.ru>
2022-09-01 17:55:20 +0300

    websocket server and spamer performed

---
+++ ws/config.py
@@ -0,0 +1,6 @@
+class Settings:
+    WS_HOST = "localhost"
+    WS_PORT = 8765
+    WS_SPAM_USER = "TEST_SPAMER"
+    SPAM_MESSAGE = "Это отдел сбербанка, ваши данные скомпрометированы!" \
+                   "сообщите пароль который придет вам по смс от 900!"
