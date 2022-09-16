051f04fb948788cd05af9bbf90fc9b4c644073dc
vitalijsofronuk <gilions@icloud.com>
2022-09-16 18:22:16 +0300

    fix mypy

--- billing/src/conf/kafka.py
+++ billing/src/conf/kafka.py
@@ -1,5 +1,6 @@
 import logging
 import pickle
+from typing import Any, ByteString, Dict
 
 from conf.helper.decorators import backoff
 from conf.settings import KAFKA_DEFAULT_PREFIX_TOPIC
@@ -20,15 +21,18 @@ class BillingKafkaProducer:
         # bootstrap_servers: 'host[:port]' string (or list of 'host[:port]'
         return KafkaProducer(bootstrap_servers=self.servers)
 
-    def push(self, topic: str, value: dict, key: str = None, **kwargs):
+    def push(self, topic: str, value: Any[Dict[str, str], ByteString], key: Any[ByteString, str] = None, **kwargs):
         if not self.producer:
             logger.warning('Kafka producer not set.')
             return
 
         if key:
-            key = key.encode('utf-8')
+            if isinstance(key, str):
+                key = key.encode('utf-8')
 
-        topic = f'{KAFKA_DEFAULT_PREFIX_TOPIC}-{topic}'
+        if isinstance(value, dict):
+            topic = f'{KAFKA_DEFAULT_PREFIX_TOPIC}-{topic}'
+
         value = pickle.dumps(value, pickle.HIGHEST_PROTOCOL)
 
         self.producer.send(topic, value=value, key=key, **kwargs)
